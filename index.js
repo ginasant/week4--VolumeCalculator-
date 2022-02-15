471aa991a57b30f8de564ab892f7db9808bf3eac
GinaSantiago <>
2022-02-15 23:58:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//jshint esversion:6
+
+// create an express object from the express package
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html")
+});
+
+app.post("/", function(req, res){
+
+  var radius = Number(req.body.n1);
+  var height = Number(req.body.n2);
+ 
+  var result = Math.PI * Math.pow(radius,2) * height;
+
+  res.send("The volume is " + result.toFixed(2));
+})
+
+app.listen(3000, function() {
+  console.log ("Server is running on port 3000")
+});
